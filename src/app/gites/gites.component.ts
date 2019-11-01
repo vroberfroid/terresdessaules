8cee1e635378125ce9c129cecab097949df87d93
Vincent Roberfroid <vincent.roberfroid@oniryx.be>
2019-11-01 19:08:30 +0100

    Addition/suppression de photos

--- src/app/gites/gites.component.ts
+++ src/app/gites/gites.component.ts
@@ -18,12 +18,11 @@ export class GitesComponent implements OnInit {
     this.imagesTB.push({source: 'assets/img/terredebas1.jpg'});
     this.imagesTB.push({source: 'assets/img/terredebas2.jpg'});
     this.imagesTB.push({source: 'assets/img/terredebas3.jpg'});
-    this.imagesTB.push({source: 'assets/img/terredebas4.jpg'});
     this.imagesTB.push({source: 'assets/img/terredebas5.jpg'});
     this.imagesTB.push({source: 'assets/img/terredebas6.jpg'});
 
     this.imagesTH = [];
-    // this.imagesTH.push({source: 'assets/img/terredehaut1.jpg'});
+    this.imagesTH.push({source: 'assets/img/terredehaut1.jpg'});
     this.imagesTH.push({source: 'assets/img/terredehaut2.jpg'});
     this.imagesTH.push({source: 'assets/img/terredehaut3.jpg'});
   }
